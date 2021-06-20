a4d995401474aa86c831b83b4e8d8c450e58728c
Nina Reed <ninareed76@gmail.com>
2021-06-20 10:55:39 -0400

    Presentational and Container Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -22,23 +22,28 @@ class CampsiteInfo extends Component {
             return (
                 <div className="col-md-5 m-1">
                     <h4>Comments</h4>
-                    {comments.map(comment => (
-                        <div key={comment.id}>{comment.text}<p>-- {comment.author}, {Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p></div>
-                    )
-                    )}
-                    
+                    {comments.map(comment => {
+                        return (
+                            <div key={comment.id}>
+                                <p>{comment.text}<br />
+                                    -- {comment.author}, {Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
+                            </div>
+                        );
+                    })} 
                 </div>
             );
         }
-        return (<div />)
+        return <div />
     }
 
     render() {
         if (this.props.campsite) {
             return (
-                <div className="row">
-                    { this.renderCampsite(this.props.campsite) }
-                    { this.renderComments(this.props.campsite.comments) }
+                <div className="container">
+                    <div className="row">
+                        { this.renderCampsite(this.props.campsite) }
+                        { this.renderComments(this.props.campsite.comments) }
+                    </div>
                 </div>
             );
         }
