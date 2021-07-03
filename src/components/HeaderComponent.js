cb92de670d41a33b933dbd6735f99eb8819b590c
Nina Reed <ninareed76@gmail.com>
2021-07-03 13:51:52 -0400

    Week 4 Workshop

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -88,7 +88,7 @@ class Header extends Component {
                 <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
                     <ModalHeader toggle={this.toggleModal}> Login </ModalHeader>
                     <ModalBody>
-                        <Form onSubmit={this.handLogin}>
+                        <Form onSubmit={this.handleLogin}>
                             <FormGroup>
                                 <Label htmlFor="username">Username</Label>
                                 <Input type="text" id="username" name="username" innerRef={input => this.username = input} />
